8d4c58fa8a32566ce7f698836b75593c823b7329
adityamore7000 <adi.more7000@gmail.com>
2024-02-15 10:44:21 +0530

    mongoose product model setup

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import Product from './models/product.model.js'
+const app = express()
+const port = process.env.PORT || 5000
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//routes
+app.get('/ping',(req,res)=>{
+    res.send('pong')
+})
+
+app.post('/api/products', async(req,res)=>{
+    res.send(req.url)
+})
+
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(port,()=>{
+        console.log(`Alive on http://localhost:${port}`)
+    })
+    console.log('Connected to database')
+}).catch((err)=>{
+    console.log(err)
+})
